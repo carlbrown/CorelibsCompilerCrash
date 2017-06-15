9822f70f56063b9d4dff955239521e41e535df32
Carl Brown <carl.brown1@ibm.com>
2017-06-15 15:57:45 -0500

    first commit

---
+++ CorelibsCompilerCrash/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  CorelibsCompilerCrash
+//
+//  Created by Carl Brown on 6/15/17.
+//
+
+import SwiftFoundation
+
+print("This compiles, but any syntax error causes a crash")
+
+//Add any syntax error after this to cause a crash
+
+X
